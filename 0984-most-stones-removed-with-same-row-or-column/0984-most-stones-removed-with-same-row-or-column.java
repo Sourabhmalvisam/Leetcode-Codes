0d0d187369d4e2af1dfdfa536adc5bdf7b1e0b31
Sourabhmalvisam <78964331+Sourabhmalvisam@users.noreply.github.com>
2023-11-04 11:18:43 +0530

    Time: 31 ms (46.61%), Space: 43.6 MB (82.13%) - LeetHub

---
+++ 0984-most-stones-removed-with-same-row-or-column/0984-most-stones-removed-with-same-row-or-column.java
@@ -0,0 +1,60 @@
+class Solution {
+    public int removeStones(int[][] stones) {
+        int n = stones.length;
+        UnionFind uf = new UnionFind(n);
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (stones[i][0] == stones[j][0] || stones[i][1] == stones[j][1]) {
+                    uf.union(i, j);
+                }
+            }
+        }
+
+        return n - uf.count();
+    }
+}
+
+class UnionFind {
+    int[] parent;
+    int[] rank;
+    int count;
+
+    public UnionFind(int n) {
+        parent = new int[n];
+        rank = new int[n];
+        count = n;
+
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+        }
+    }
+
+    public int find(int x) {
+        if (x != parent[x]) {
+            parent[x] = find(parent[x]);
+        }
+        return parent[x];
+    }
+
+    public void union(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+
+        if (rootX != rootY) {
+            if (rank[rootX] < rank[rootY]) {
+                parent[rootX] = rootY;
+            } else if (rank[rootX] > rank[rootY]) {
+                parent[rootY] = rootX;
+            } else {
+                parent[rootX] = rootY;
+                rank[rootY]++;
+            }
+            count--;
+        }
+    }
+
+    public int count() {
+        return count;
+    }
+}
